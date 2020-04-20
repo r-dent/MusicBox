6ba5aeb7114435ce1aea9e73b04416c9b0d0546b
Roman Gille <developer@romangille.com>
2020-04-20 16:44:24 +0200

    Read text from SD card

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,45 @@
 #include <Arduino.h>
 
 #include <SPI.h>
-// #include <SD.h>
+#include <SD.h>
 #include <MFRC522.h>
 
+void printDirectory(File dir, int numTabs);
+void printFile(String path);
+void useSDCard(bool shouldUse);
+void useRFID(bool shouldUse);
+
 const int RST_PIN = 22; // Reset pin
-const int SS_PIN = 21; // Slave select pin
+const int RFID_SELECT_PIN = 21; 
+const int SD_SELECT_PIN = 5;
+
+File sdCardRoot;
  
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance 
+MFRC522 mfrc522(RFID_SELECT_PIN, RST_PIN); // Create MFRC522 instance 
 
 void setup()
 {
- // Open serial communications and wait for port to open:
+  pinMode(RFID_SELECT_PIN, OUTPUT);
+  pinMode(SD_SELECT_PIN, OUTPUT);
+
+  // Open serial communications and wait for port to open:
   Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
-  // Serial.print("Initializing SD card...");
-  // if (!SD.begin(5)) {
-  //   Serial.println("initialization failed!");
-  //   while (1);
-  // }
-  // Serial.println("initialization done.");
+  useSDCard(true);
+
+  Serial.print("Initializing SD card...");
+  if (SD.begin(5)) {
+    Serial.println("initialization done.");
+    sdCardRoot = SD.open("/");
+    printDirectory(sdCardRoot, 0);
+  } else {
+    Serial.println("initialization failed!");
+  }
+
+  useRFID(true);
 
   SPI.begin(); // Init SPI bus
   mfrc522.PCD_Init(); // Init MFRC522
@@ -31,6 +48,8 @@ void setup()
 }
 
 void loop(void) {
+
+  useRFID(true);
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
@@ -42,21 +61,21 @@ void loop(void) {
     return;
   }
 
-  Serial.print("UID tag :");
+  Serial.print("UID tag:");
   String content= "";
-  byte letter;
+  
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-    //  Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    //  Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   content.toUpperCase();
   Serial.println(content);
-  Serial.println(content.substring(1));
+
   if (content.substring(1) == "69 E9 FA 63") {
     Serial.println("Authorized access");
+    useSDCard(true);
+    printFile("/info.txt");
     delay(3000);
   } else {
     Serial.println("Access denied");
@@ -65,4 +84,60 @@ void loop(void) {
   
   // Dump debug info about the card; PICC_HaltA() is automatically called
   // mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}
+
+// SD Card methods.
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
+}
+
+void printFile(String path) {
+  File file = SD.open(path);
+  if (file) {
+    Serial.println(path);
+    
+    // read from the file until there's nothing else in it:
+    while (file.available()) {
+    	Serial.write(file.read());
+    }
+    // close the file:
+    file.close();
+  } else {
+  	// if the file didn't open, print an error:
+    Serial.println("error opening ");
+    Serial.print(path);
+  }
+}
+
+// SPI related.
+
+void useSDCard(bool shouldUse) {
+  digitalWrite(RFID_SELECT_PIN, HIGH);
+  digitalWrite(SD_SELECT_PIN, (shouldUse ? LOW : HIGH));
+}
+
+void useRFID(bool shouldUse) {
+  digitalWrite(SD_SELECT_PIN, HIGH);
+  digitalWrite(RFID_SELECT_PIN, (shouldUse ? LOW : HIGH));
 }
